ce2db2aa904597931f9589910243402394b7fbae
Oscar <oscar.marafko@gmail.com>
2018-04-18 21:54:01 +0200

    Add files via upload

--- BBB_I2C+Broker.py
+++ BBB_I2C+Broker.py
@@ -10,6 +10,8 @@ topicName = "topic/1f04";
 #sets the adress of the device on the I2C bus
 deviceName = "0x68";
 
+picGoToSleep = False;
+
 mqttc = mqtt.Client();
 #connects to the hub
 mqttc.connect("172.17.4.95");
@@ -17,12 +19,33 @@ message = "";
 #connects to the divice 0x08 on the I2C-2 bus
 i2c = I2C.get_i2c_device(deviceName, 2);
 
-while True;
+#experimental because never tested
+#allows the user to toggle on/off the data sampling from the dash-board
+'''
+def on_connect(mqttc, userdata, flags, rc):
+	mqttc.subscribe(topicName)
+
+def on_message(mqttc, userdata, msg):
+	if msg.payload.decode() == "sleep":
+		picGoToSleep = True;
+		mqttc.disconnect()
+	if msg.payload.decode() == "wakeup":
+		picGoToSleep = False;
+    	mqttc.disconnect()
+
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+mqttc.loop_start()
+'''
+
+while picGoToSleep = False;
 	#sends the 8 bits signal to the device to send the temperature back
 	I2C.Device.writeRaw8(i2c, 0X01)
 	#reads a byte on the I2C bus
 	byte = I2C.Device.readRaw8(i2c);
 	message = str(byte);
-	#sends the byte on the hub
+	#sends the temperature on the hub
 	mqttc.publish(topicName, message);
 	time.sleep(1);
+
+time.sleep(1)
